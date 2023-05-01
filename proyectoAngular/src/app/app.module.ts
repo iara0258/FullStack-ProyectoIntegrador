776f4b7d5ca91c79411f2c6f7d0dd77a223599e7
Soledad Quiroz <soledadquiroz96@gmail.com>
2023-05-01 18:24:54 -0300

    Se agrega componente Registro.

--- proyectoAngular/src/app/app.module.ts
+++ proyectoAngular/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConsejosComponent } from './consejos/consejos.component';
 import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ConsejosComponent,
-    LoginComponent
+    LoginComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
